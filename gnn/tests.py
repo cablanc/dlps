1791c296b18e57f27dd77a85163ff348e1ebcd71
christine <allec@seas.upenn.edu>
2021-01-20 14:12:10 -0500

    Adds functions for computing social term and drift
    Some functions that were defined in the notebook are now in the utils.py file. A tests.py file is added without tests.

---
+++ gnn/tests.py
@@ -0,0 +1,5 @@
+import networkx as nx
+
+
+def test_build_all_to_all():
+    pass
--- gnn/utils.py
+++ gnn/utils.py
@@ -113,3 +113,61 @@ def build_all_to_all(num_nodes):
     # print(edges)
     
     return G
+
+
+def build_edge_trace(edge_x, edge_y):
+    
+    edge_trace = go.Scatter(
+    x=edge_x, y=edge_y,
+    line=dict(width=0.5, color='#888'),
+    hoverinfo='none',
+    mode='lines')
+    
+    return edge_trace
+
+
+def build_node_trace(node_x, node_y):
+    
+    node_trace = go.Scatter(
+    x=node_x, y=node_y,
+    mode='markers',
+    hoverinfo='text',
+    marker=dict(
+        showscale=True,
+        # colorscale options
+        #'Greys' | 'YlGnBu' | 'Greens' | 'YlOrRd' | 'Bluered' | 'RdBu' |
+        #'Reds' | 'Blues' | 'Picnic' | 'Rainbow' | 'Portland' | 'Jet' |
+        #'Hot' | 'Blackbody' | 'Earth' | 'Electric' | 'Viridis' |
+        colorscale='YlGnBu',
+        reversescale=True,
+        color=[],
+        size=10,
+        colorbar=dict(
+            thickness=15,
+            title='Node Connections',
+            xanchor='left',
+            titleside='right'
+        ),
+        line_width=2))
+    
+    return node_trace
+
+
+def build_graph_viz(edge_trace, node_trace):
+    
+    fig = go.Figure(data=[edge_trace, node_trace],
+             layout=go.Layout(
+                title='<br>Network graph made with Python',
+                titlefont_size=16,
+                showlegend=False,
+                hovermode='closest',
+                margin=dict(b=20,l=5,r=5,t=40),
+                annotations=[ dict(
+                    text="Python code: <a href='https://plotly.com/ipython-notebooks/network-graphs/'> https://plotly.com/ipython-notebooks/network-graphs/</a>",
+                    showarrow=False,
+                    xref="paper", yref="paper",
+                    x=0.005, y=-0.002 ) ],
+                xaxis=dict(showgrid=False, zeroline=False, showticklabels=False),
+                yaxis=dict(showgrid=False, zeroline=False, showticklabels=False))
+                )
+    return fig
